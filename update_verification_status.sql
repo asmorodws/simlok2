e644909ba484dc128df3fd53d0a252c9ddf9200c
moro <asmorodwi11@gmail.com>
2025-10-01 07:59:35 +0700

    bersih bersih

--- update_verification_status.sql
+++
@@ -1,5 +0,0 @@
--- Update all ADMIN users to SUPER_ADMIN role before migration
-UPDATE User SET role = 'SUPER_ADMIN' WHERE role = 'ADMIN';
-
--- Update verified_by references from ADMIN to SUPER_ADMIN  
-UPDATE User SET verified_by = 'SUPER_ADMIN' WHERE verified_by = 'ADMIN';
